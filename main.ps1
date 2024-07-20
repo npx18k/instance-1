ccf6af837c6400b9448a2d00fd4af33002e5d096
npx18k <157927691+npx18k@users.noreply.github.com>
2024-07-21 01:36:38 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,3 @@
 # Credits By NEURS
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-echo downloaded .msi
