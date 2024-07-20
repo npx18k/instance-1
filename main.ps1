35c2412503b283961da94776256f0b3e4db93596
npx18k <157927691+npx18k@users.noreply.github.com>
2024-07-21 01:03:26 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,8 @@
 # Credits By NEURS
+echo Setup firewall rule...
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+echo Success!
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+echo downloaded .msi
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+echo downloaded .exe
