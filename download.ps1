fb03911b870532712246a3a104ef565591595c00
npx18k <157927691+npx18k@users.noreply.github.com>
2024-08-04 13:45:16 +0300

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,12 +1,12 @@
 Invoke-WebRequest -Uri https://eu1.llaun.ch/jar -OutFile $env:userprofile\Downloads\launcher.jar
-Invoke-WebRequest -Uri https://github.com/pal1000/mesa-dist-win/releases/download/24.1.4/mesa3d-24.1.4-release-msvc.7z -OutFile $env:userprofile\Downloads\drivers.7z
+Invoke-WebRequest -Uri https://github.com/pal1000/mesa-dist-win/releases/download/24.1.5/mesa3d-24.1.5-release-msvc.7z -OutFile $env:userprofile\Downloads\drivers.7z
 Invoke-WebRequest -Uri https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.0.1/fabric-installer-1.0.1.jar -OutFile $env:userprofile\Downloads\fabric-installer-1.0.1.jar
 choco install temurinjre -y
 choco install 7zip.install -y
 
 mkdir $env:appdata\.minecraft\mods\
-Invoke-WebRequest -Uri https://cdn.modrinth.com/data/P7dR8mSH/versions/GT0R5Mz7/fabric-api-0.100.4%2B1.20.6.jar -OutFile $env:appdata\.minecraft\mods\api.jar
-Invoke-WebRequest -Uri https://cdn.modrinth.com/data/gvQqBUqZ/versions/5a3sPIH2/lithium-fabric-mc1.20.6-0.12.5.jar -OutFile $env:appdata\.minecraft\mods\lithium.jar
-Invoke-WebRequest -Uri https://cdn.modrinth.com/data/NNAgCjsB/versions/6U5rh26a/entityculling-fabric-1.6.6-mc1.20.6.jar -OutFile $env:appdata\.minecraft\mods\entityculling.jar
-Invoke-WebRequest -Uri https://cdn.modrinth.com/data/AANobbMI/versions/OwLQelEI/sodium-fabric-0.5.11%2Bmc1.20.6.jar -OutFile $env:appdata\.minecraft\mods\sodium.jar
-Invoke-WebRequest -Uri https://github.com/Pan4ur/ThunderHack-Recode/releases/download/latest/thunderhack-1.6.jar -OutFile $env:appdata\.minecraft\mods\thunderhack-1.6.jar
+Invoke-WebRequest -Uri https://cdn.modrinth.com/data/P7dR8mSH/versions/iS2jNAxk/fabric-api-0.100.8%2B1.21.jar -OutFile $env:appdata\.minecraft\mods\api.jar
+Invoke-WebRequest -Uri https://cdn.modrinth.com/data/AANobbMI/versions/RncWhTxD/sodium-fabric-0.5.11%2Bmc1.21.jar -OutFile $env:appdata\.minecraft\mods\sodium.jar
+Invoke-WebRequest -Uri https://github.com/sakura-ryoko/litematica/releases/download/1.21-0.19.0/litematica-fabric-1.21-0.19.0.jar -OutFile $env:appdata\.minecraft\mods\litematica.jar
+Invoke-WebRequest -Uri https://cdn.modrinth.com/data/ITCqumHN/versions/QdRNHpMc/litematica-printer-mc1.21-7.2.0.jar -OutFile $env:appdata\.minecraft\mods\printer.jar
+Invoke-WebRequest -Uri https://maven.meteordev.org/snapshots/meteordevelopment/baritone/1.21-SNAPSHOT/baritone-1.21-20240721.143401-1.jar -OutFile $env:appdata\.minecraft\mods\baritone.jar
